f8ba3e19b98b0b4b3854723d7ec794e8c8bb0476
Andreas Verheijen <averheij@student.codam.nl>
2019-09-13 16:46:42 +0200

    fixed exercise 07

--- ex07/ft_strrev.c
+++ ex07/ft_strrev.c
@@ -6,7 +6,7 @@
 /*   By: averheij <averheij@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/09/12 13:50:28 by averheij       #+#    #+#                */
-/*   Updated: 2019/09/13 15:58:09 by averheij      ########   odam.nl         */
+/*   Updated: 2019/09/13 16:43:36 by averheij      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,29 +20,27 @@ char	*ft_strrev(char *str)
 	int		count;
 	int		length;
 	char	swap;
-	char	*newstart;
 
-	swap = 'z';
-	newstart = &swap + sizeof(char);
 	count = 0;
 	length = 0;
 	while (str[length] != '\0')
 	{
 		length = length + 1;
 	}
-	printf("%s", "hi");
-	while (count < length)
+	while (count < (length / 2))
 	{
-		*(newstart + count) = str[length - 1 - count];
+		swap = str[count];
+		str[count] = str[length - 1 - count];
+		str[length - 1 - count] = swap;
 		count = count + 1;
 	}
-	str = newstart;
 	return (str);
 }
 /*fend */
 
 int		main(void)
 {
-	printf("%s", ft_strrev("hellomate"));
+	char test[] = "test";
+	printf("%s", ft_strrev(test));
 	return (0);
 }
